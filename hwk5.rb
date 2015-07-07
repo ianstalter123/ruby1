df9ab8f686d7a5e46bd74f8631f601b01e5c47e8
ianstalter123 <istalter66@gmail.com>
2015-07-06 21:41:12 -0700

    added headers

--- hwk5.rb
+++ hwk5.rb
@@ -1,13 +1,28 @@
 total = 9
 i = 0
 str = ""
+line = ""
 h = 1
 w = 1
+a = 1
 
 puts "a multiplication table:"
 
-while w <= 9
+while a <= 9
 
+x = a.to_s
+str += "  %-3d" % x
+line += "-----"
+
+a += 1
+end
+puts str
+puts line
+
+str = ""
+while w <= 9
+x = w.to_s
+str += x + "|"
 	while h <= 9
 		x = (w*h).to_s
 		#well it works somehow for alignment!
